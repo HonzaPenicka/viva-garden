035b8e0cf5de920e64c2e0be053a47e758faf481
Servant Salamander <servantsalamander@Servant--MacBook-Air.local>
2025-10-09 20:40:56 +0200

    contact

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,100 +1,79 @@
 export const ContactForm = () => {
   return (
     <form>
-      <div className="grid gap-4 mb-4 md:grid-cols-2 border-yellow-700/50 border-2 p-4 rounded-xl">
+      <div className="grid gap-4 mb-4 md:grid-cols-2 border-green-700 border-2 p-4 rounded-xl">
         <div>
-          <label className="block mb-2 text-sm font-medium text-black">
-            First name
+          <label className="block mb-2 text-sm font-semibold text-black">
+            Jméno
           </label>
           <input
             type="text"
             id="first_name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="John"
+            className="bg-green-50 border border-green-700/50 text-black text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2 placeholder-yellow-700/50  dark:focus:ring-green-600 dark:focus:border-green-600"
+            placeholder="Jaromír"
             required
           />
         </div>
+
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Last name
+          <label className="block mb-2 text-sm font-semibold text-black">
+            Příjmení
           </label>
           <input
             type="text"
             id="last_name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Doe"
+            className="bg-green-50 border border-green-700/50 text-black text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2   placeholder-yellow-700/50  dark:focus:ring-green-600 dark:focus:border-green-600"
+            placeholder="Novák"
             required
           />
         </div>
+
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Company
+          <label className="block mb-2 text-sm font-semibold text-black">
+            Společnost
           </label>
           <input
             type="text"
             id="company"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Flowbite"
-            required
+            className="bg-green-50 border border-green-700/50 text-black text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2   placeholder-yellow-700/50  dark:focus:ring-green-600 dark:focus:border-green-600"
+            placeholder="Lesy s.r.o."
           />
         </div>
+
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Phone number
+          <label className="block mb-2 text-sm font-semibold text-black">
+            Telefonní číslo
           </label>
           <input
             type="tel"
             id="phone"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="123-45-678"
+            className="bg-green-50 border border-green-700/50 text-black text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2   placeholder-yellow-700/50  dark:focus:ring-green-600 dark:focus:border-green-600"
+            placeholder="+420 123 456 789"
             pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
             required
           />
         </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Website URL
-          </label>
-          <input
-            type="url"
-            id="website"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="flowbite.com"
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Unique visitors (per month)
-          </label>
-          <input
-            type="number"
-            id="visitors"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder=""
-            required
-          />
-        </div>
-      </div>
-      <div className="mb-6">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Email address
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="john.doe@company.com"
-          required
-        />
-      </div>
 
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Submit
-      </button>
+        <div className="mb-6">
+          <label className="block mb-2 text-sm font-semibold text-black">
+            Emailová adresa
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-green-50 border border-green-700/50 text-black text-sm rounded-lg focus:ring-green-600 focus:border-green-600 block w-full p-2   placeholder-yellow-700/50  dark:focus:ring-green-600 dark:focus:border-green-600"
+            placeholder="jaromir.novak@gmail.com"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-green-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-green-600 font-semibold rounded-xl text-sm w-full sm:w-auto px-5 py-2 text-center dark:bg-green-600 dark:hover:bg-green-600 dark:focus:ring-green-600"
+        >
+          Odeslat
+        </button>
+      </div>
     </form>
   );
 };
